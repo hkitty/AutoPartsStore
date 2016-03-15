1d8b91a05d322448dee2f9b743a009ae5234e1a6
hkitty <hkityy1488@gmail.com>
2016-03-15 19:10:55 +0200

    Add login window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,10 +5,62 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    accountsDB.setDatabaseName("Accounts.sqlite");
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    exit(1);
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    if ( !ui->loginEdit->text().isEmpty() && !ui->passwordEdit->text().isEmpty() ) {
+        login = ui->loginEdit->text();
+        password = ui->passwordEdit->text();
+
+        switch (isLogin(login, password)) {
+        case 0:
+            break;
+        case 254:
+            QMessageBox::critical(0, "Login error", "User not found!");
+            break;
+        case 255:
+            QMessageBox::critical(0, "Database error", "Check database connection!");
+            break;
+        default:
+            QMessageBox::critical(0, "Login error", "Unknown error!");
+            break;
+        }
+    } else {
+        QMessageBox::critical(0, "Login error", "Fill in all the fields!");
+    }
+}
+
+int MainWindow::isLogin(QString userLogin, QString userPassword)
+{
+    if ( !accountsDB.open() ) {
+        return 255;
+    } else {
+        QSqlQuery loginQuery(accountsDB);
+
+        loginQuery.prepare("SELECT accessRights FROM Users WHERE login=:login AND password=:password");
+        loginQuery.bindValue(":login", userLogin);
+        loginQuery.bindValue(":password", userPassword);
+
+        if ( !loginQuery.exec() ) {
+            qDebug() << "Login error: " << loginQuery.lastError().text();
+            return 254;
+        } else {
+            while ( loginQuery.next() ) {
+                return loginQuery.value(0).toInt();
+            }
+        }
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox>
+#include <QtSql>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,20 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_actionExit_triggered();
+
+    void on_pushButton_clicked();
+
+    int isLogin(QString userLogin, QString userPassword);
+
 private:
     Ui::MainWindow *ui;
+
+    QString login;
+    QString password;
+
+    QSqlDatabase accountsDB = QSqlDatabase::addDatabase("QSQLITE");
 };
 
 #endif // MAINWINDOW_H
